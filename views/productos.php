1b4b29ad48dea16ed760444b5d794393b1ae15e1
Alejandro Rivera <jarivera636@misena.edu.co>
2021-06-10 15:12:02 -0500

    Se agrego las card de los productos

--- views/productos.php
+++ views/productos.php
@@ -5,11 +5,12 @@ for ($i = 0; $i < 8; $i++) {
 
 
 ?>
+<!--Cards de los productos - comida-->
     <div class="m-2">
         <div class="card" style="width: 18rem;">
             <img src="../access/img/fastFood/food_<?php echo $i; ?>.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text text-center">Delicious fast food</p>
             </div>
         </div>
     </div>
