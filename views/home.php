949b526ee02beb04bd93434a57637192159bc73d
Alejandro Rivera <jarivera636@misena.edu.co>
2021-06-09 22:38:09 -0500

    app 06 iniciada se creo la vista home ver video min 1526

---
+++ views/home.php
@@ -0,0 +1,13 @@
+<!--Vista Home principal-->
+<H1 class="h3 mb-3">PAGINA PRINCIPAL</H1>
+
+       <div class="col-12 d-flex flex-column align-items-center flex-lg-row justify-content-lg-around">
+
+            <div class="col-12 col-md-6 col-lg-3 bg-light rounded rounded p-2 m-3 m-lg-0"><img class="img-fluid" src="../access/img/imgDeveloper1.jpg" alt=""></div>
+
+            <div class="col-12 col-md-6 col-lg-8 bg-light rounded rounded p-5">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus, mollitia itaque odio ea temporibus neque facilis. Eum, sint dolore aliquid quibusdam assumenda quasi illo, tempore cupiditate saepe consectetur, accusamus ab?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente culpa, similique possimus eos odio pariatur consequatur veniam nobis earum quas incidunt asperiores itaque delectus repudiandae rem dolores quisquam debitis aliquam!</p>
+            </div>
+
+       </div>
---
+++ views/layout.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../access/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../access/css/styles.css">
+    <title>Taller Bootstrap</title>
+</head>
+<body>
+     <!--HEADER-->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">PHP</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Nosotros</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Productos</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <!--MAIN-->  
+    <main class="col-12 bg-primary d-flex p-4 d-flex flex-column align-items-center">
+       <!--Aqui va codigo php-->
+       <?php
+
+        if (!isset($vista)) {
+            
+            $vista = "home.php";
+        }
+         require $vista;
+
+       ?>
+
+    </main>
+
+    <!--FOOTER-->
+    <footer class="bg-dark d-flex justify-content-center align-items-center">
+
+        <div class="d-flex justify-content-lg-around align-items-center m-2">
+            <div class="m-2"><img class="imgGithub" src="../access/img/LogoGit.png" alt=""></div>
+            <div class="d-flex flex-column align-items-center">
+                <p class="text-white m-0 p-0">Alejandro Rivera</p>
+                <p class="text-white m-0 p-0">AlejoR96</p>
+            </div>
+        </div>
+    
+    </footer>
+    <!--SCRIPT JS-->
+    <script> src="../access/js/bootstrap.bundle.min.js"</script>
+</body>
+</html>
